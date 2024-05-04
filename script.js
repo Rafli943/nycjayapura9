0da91041990c36a5e2017c91abe040ba7f3874b0
Rafli Aditya <rafliap943@gmail.com>
2024-05-04 09:54:53 +0700

    menambahkan contact section

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const menu = document.querySelector('.menu');
 
+    // Tambah event listener pada hamburgerMenu untuk toggle menu
     hamburgerMenu.addEventListener('click', function () {
         menu.classList.toggle('active');
     });
-});
 
-document.addEventListener('DOMContentLoaded', function () {
+    // Tambah event listener pada setiap tautan di dalam menu
     const menuLinks = document.querySelectorAll('.menu a');
-
     menuLinks.forEach(function (link) {
         link.addEventListener('click', function (event) {
             event.preventDefault();
@@ -21,7 +20,23 @@ document.addEventListener('DOMContentLoaded', function () {
                 targetSection.scrollIntoView({
                     behavior: 'smooth'
                 });
+
+                // Tutup menu hamburger setelah tautan diklik
+                menu.classList.remove('active');
             }
         });
+    });
+
+    // Tambah event listener untuk menutup menu saat mengklik di luar menu
+    document.addEventListener('click', function (event) {
+        const targetElement = event.target;
+
+        // Periksa apakah yang diklik bukan bagian dari menu atau hamburgerMenu
+        if (!targetElement.closest('.menu') && !targetElement.closest('.hamburger-menu')) {
+            // Tutup menu jika sedang terbuka
+            if (menu.classList.contains('active')) {
+                menu.classList.remove('active');
+            }
+        }
     });
 });
